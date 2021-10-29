2cc8927946a212461647617661edb7ae19618e97
guiiivale <guigato62@gmail.com>
2021-10-28 22:49:49 -0300

    Feature(Second commit): Added views, bootstrap and controllers

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,33 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <ul class="nav">
+            <li class="nav-item">
+                <a href="https://creative-tim.com" target="blank" class="nav-link">
+                    {{ __('Creative Tim') }}
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="https://updivision.com" target="blank" class="nav-link">
+                    {{ __('Updivision') }}
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    {{ __('About Us') }}
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    {{ __('Blog') }}
+                </a>
+            </li>
+        </ul>
+        <div class="copyright">
+            &copy; {{ now()->year }} {{ __('made with') }} <i class="tim-icons icon-heart-2"></i> {{ __('by') }}
+            <a href="https://github.com/guiiivale" target="_blank">{{ __('Guilherme Vale') }}</a>{{','}} 
+            <a href="https://github.com/JonathanMeira" target="_blank">{{ __('Jonathan Meira') }}</a> {{','}} 
+            <a href="https://github.com/pauloportugal1" target="_blank">{{ __('Paulo Portugal') }}</a> {{__('and ')}} 
+            <a href="https://github.com/Gulama" target="_blank">{{ __('Gustavo Benites') }}</a> 
+        </div>
+    </div>
+</footer>
