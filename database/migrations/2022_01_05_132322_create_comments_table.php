99d69e6af248c5657c60635890d9d99dc1f1bf0a
Guilherme Vale <guilhermevale@Guilhermes-MacBook-Pro.local>
2022-01-05 10:42:28 -0300

    Updated posts table, created comments, sites and categories table

---
+++ database/migrations/2022_01_05_132322_create_comments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedbigInteger('user_id');
+            $table->unsignedbigInteger('post_id');
+            $table->mediumText('text');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('post_id')->references('id')->on('posts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
