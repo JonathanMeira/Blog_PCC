a4db323588c5355dfd211ab179348c5cf58f2643
Jonathan Meira <jonathan_castilho_meira@hotmail.com>
2022-03-12 17:32:08 -0300

    ref - exclusão de componentes que não serão utilizados

--- resources/views/layouts/category.blade.php
+++ resources/views/layouts/category.blade.php
@@ -1,36 +1,16 @@
-@extends('layouts.main')
-@section('title', 'Insira Nome - Categoria')
+@extends('layouts.main', [ 'pageSlug' => 'category'])
+
+@section('title', config('app.name'))
 
 @section('content')
 <section>
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <!-- Breadcrumb -->
-                <ul class="breadcrumbs bg-light mb-4">
-                    <li class="breadcrumbs__item">
-                        <a href="index.html" class="breadcrumbs__url">
-                            <i class="fa fa-home"></i> Home</a>
-                    </li>
-                    <li class="breadcrumbs__item">
-                        <a href="index.html" class="breadcrumbs__url">News</a>
-                    </li>
-                    <li class="breadcrumbs__item breadcrumbs__item--current">
-                        World
-                    </li>
-                </ul>
-            </div>
-
-        </div>
-    </div>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8">
                 <aside class="wrapper__list__article ">
-                    <h4 class="border_section">Category title</h4>
-
+                    <h4 class="border_section">Nome da categoria</h4>
                     <div class="row">
-                        <div class="col-md-6">
+                        <div class="col-md-3">
                             <!-- Post Article -->
                             <div class="article__entry">
                                 <div class="article__image">
@@ -178,7 +158,7 @@
 
 
                         </div>
-                        <div class="col-md-6">
+                        <div class="col-md-3">
                             <!-- Post Article -->
                             <div class="article__entry">
                                 <div class="article__image">
@@ -330,194 +310,7 @@
                 </aside>
 
             </div>
-            <div class="col-md-4">
-                <div class="sidebar-sticky">
-                    <aside class="wrapper__list__article ">
-                        <h4 class="border_section">Sidebar</h4>
-                        <div class="wrapper__list__article-small">
-                            <div class="mb-3">
-                                <!-- Post Article -->
-                                <div class="card__post card__post-list">
-                                    <div class="image-sm">
-                                        <a href="./card-article-detail-v1.html">
-                                            <img src="images/placeholder/500x400.jpg" class="img-fluid" alt="">
-                                        </a>
-                                    </div>
-
-
-                                    <div class="card__post__body ">
-                                        <div class="card__post__content">
-
-                                            <div class="card__post__author-info mb-2">
-                                                <ul class="list-inline">
-                                                    <li class="list-inline-item">
-                                                        <span class="text-primary">
-                                                            by david hall
-                                                        </span>
-                                                    </li>
-                                                    <li class="list-inline-item">
-                                                        <span class="text-dark text-capitalize">
-                                                            descember 09, 2016
-                                                        </span>
-                                                    </li>
-
-                                                </ul>
-                                            </div>
-                                            <div class="card__post__title">
-                                                <h6>
-                                                    <a href="./card-article-detail-v1.html">
-                                                        6 Best Tips for Building a Good Shipping Boat
-                                                    </a>
-                                                </h6>
-                                                <!-- <p class="d-none d-lg-block d-xl-block">
-                    Maecenas accumsan tortor ut velit pharetra mollis. Proin eu nisl et arcu iaculis placerat
-                    sollicitudin ut est. In fringilla dui dui.
-                </p> -->
-
-                                            </div>
-
-                                        </div>
-
-
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="mb-3">
-                                <!-- Post Article -->
-                                <div class="card__post card__post-list">
-                                    <div class="image-sm">
-                                        <a href="./card-article-detail-v1.html">
-                                            <img src="images/placeholder/500x400.jpg" class="img-fluid" alt="">
-                                        </a>
-                                    </div>
-
-
-                                    <div class="card__post__body ">
-                                        <div class="card__post__content">
-
-                                            <div class="card__post__author-info mb-2">
-                                                <ul class="list-inline">
-                                                    <li class="list-inline-item">
-                                                        <span class="text-primary">
-                                                            by david hall
-                                                        </span>
-                                                    </li>
-                                                    <li class="list-inline-item">
-                                                        <span class="text-dark text-capitalize">
-                                                            descember 09, 2016
-                                                        </span>
-                                                    </li>
-
-                                                </ul>
-                                            </div>
-                                            <div class="card__post__title">
-                                                <h6>
-                                                    <a href="./card-article-detail-v1.html">
-                                                        6 Best Tips for Building a Good Shipping Boat
-                                                    </a>
-                                                </h6>
-                                                <!-- <p class="d-none d-lg-block d-xl-block">
-                    Maecenas accumsan tortor ut velit pharetra mollis. Proin eu nisl et arcu iaculis placerat
-                    sollicitudin ut est. In fringilla dui dui.
-                </p> -->
-
-                                            </div>
-
-                                        </div>
-
-
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="mb-3">
-                                <!-- Post Article -->
-                                <div class="card__post card__post-list">
-                                    <div class="image-sm">
-                                        <a href="./card-article-detail-v1.html">
-                                            <img src="images/placeholder/500x400.jpg" class="img-fluid" alt="">
-                                        </a>
-                                    </div>
-
-
-                                    <div class="card__post__body ">
-                                        <div class="card__post__content">
-
-                                            <div class="card__post__author-info mb-2">
-                                                <ul class="list-inline">
-                                                    <li class="list-inline-item">
-                                                        <span class="text-primary">
-                                                            by david hall
-                                                        </span>
-                                                    </li>
-                                                    <li class="list-inline-item">
-                                                        <span class="text-dark text-capitalize">
-                                                            descember 09, 2016
-                                                        </span>
-                                                    </li>
-
-                                                </ul>
-                                            </div>
-                                            <div class="card__post__title">
-                                                <h6>
-                                                    <a href="./card-article-detail-v1.html">
-                                                        6 Best Tips for Building a Good Shipping Boat
-                                                    </a>
-                                                </h6>
-                                                <!-- <p class="d-none d-lg-block d-xl-block">
-                    Maecenas accumsan tortor ut velit pharetra mollis. Proin eu nisl et arcu iaculis placerat
-                    sollicitudin ut est. In fringilla dui dui.
-                </p> -->
-
-                                            </div>
-
-                                        </div>
-
-
-                                    </div>
-                                </div>
-                            </div>
-
-                            <!-- Post Article -->
-                            <div class="article__entry">
-                                <div class="article__image">
-                                    <a href="#">
-                                        <img src="images/placeholder/500x400.jpg" alt="" class="img-fluid">
-                                    </a>
-                                </div>
-                                <div class="article__content">
-                                    <div class="article__category">
-                                        travel
-                                    </div>
-                                    <ul class="list-inline">
-                                        <li class="list-inline-item">
-                                            <span class="text-primary">
-                                                by david hall
-                                            </span>
-                                        </li>
-                                        <li class="list-inline-item">
-                                            <span class="text-dark text-capitalize">
-                                                descember 09, 2016
-                                            </span>
-                                        </li>
-
-                                    </ul>
-                                    <h5>
-                                        <a href="#">
-                                            Proin eu nisl et arcu iaculis placerat sollicitudin ut est
-                                        </a>
-                                    </h5>
-                                    <p>
-                                        Maecenas accumsan tortor ut velit pharetra mollis. Proin eu nisl et arcu iaculis placerat sollicitudin ut
-                                        est. In fringilla dui dui.
-                                    </p>
-                                    <a href="#" class="btn btn-outline-primary mb-4 text-capitalize"> read more</a>
-                                </div>
-                            </div>
-                        </div>
-                    </aside>
-
-
-            <div class="clearfix"></div>
+    <div class="clearfix"></div>
         </div>
         <!-- Pagination -->
         <div class="pagination-area">
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <div class="container-fluid">
         <div class="copyright">
-            &copy; {{ now()->year }} {{ __('made with') }} <i class="tim-icons icon-heart-2"></i> {{ __('by') }}
+            &copy; {{ now()->year }} {{ __('made with') }} <i class="fa fa-heart"></i> {{ __('by') }}
             <a href="https://github.com/guiiivale" target="_blank">{{ __('Guilherme Vale') }}</a>{{','}} 
             <a href="https://github.com/JonathanMeira" target="_blank">{{ __('Jonathan Meira') }}</a> {{','}} 
             <a href="https://github.com/pauloportugal1" target="_blank">{{ __('Paulo Portugal') }}</a> {{__('and ')}} 
