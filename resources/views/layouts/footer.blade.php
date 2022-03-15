5c6c2ed2a34017b479861a3796ef4ceef70bb223
gulama14 <guru14bw@gmail.com>
2022-03-15 13:16:44 -0300

    arrumei meu link do github no footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -5,7 +5,7 @@
             <a href="https://github.com/guiiivale" target="_blank">{{ __('Guilherme Vale') }}</a>{{','}} 
             <a href="https://github.com/JonathanMeira" target="_blank">{{ __('Jonathan Meira') }}</a> {{','}} 
             <a href="https://github.com/pauloportugal1" target="_blank">{{ __('Paulo Portugal') }}</a> {{__('and ')}} 
-            <a href="https://github.com/Gulama" target="_blank">{{ __('Gustavo Benites') }}</a> 
+            <a href="https://github.com/gulama14" target="_blank">{{ __('Gustavo Benites') }}</a> 
         </div>
     </div>
 </footer>
